8a552d44edce8720ed74c91a68df0f5a1bd7108e
Jonathan Tobias <jonathantosilva@hotmail.com>
2026-03-27 15:59:29 -0300

    chore(test): setup axe-core/playwright and production parity for e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests/e2e',
+  fullyParallel: !!process.env.CI,
   use: { baseURL: 'http://127.0.0.1:4321' },
+  expect: {
+    toHaveScreenshot: {
+      maxDiffPixelRatio: 0.05,
+    },
+  },
   webServer: {
-    command: 'pnpm astro dev --host 127.0.0.1 --port 4321',
+    command: 'pnpm preview --port 4321',
     port: 4321,
     reuseExistingServer: !process.env.CI
   }
